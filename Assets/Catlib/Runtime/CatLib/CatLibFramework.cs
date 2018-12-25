c87a6db20588fbe8ca7f0a79414a60c867a39322
xiaogeformax <xiaogeformax@gmail.com>
2018-12-25 23:47:49 +0800

    catlib application类

---
+++ Assets/Catlib/Runtime/CatLib/CatLibFramework.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CatLib
+{
+    [DisallowMultipleComponent]
+    [AddComponentMenu("CatLib/Framework")]
+    public sealed class CatLibFramework : MonoBehaviour
+    {
+        /// <summary>
+        /// Unity Start
+        /// </summary>
+        public void Start()
+        {
+            var gameBox = new UnityApplication(this);
+            gameBox.Bootstrap(GetBootstraps());
+            gameBox.Init();
+
+        }
+
+        /// <summary>
+        /// 获取引导程序
+        /// </summary>
+        /// <returns>引导脚本</returns>
+        private IBootstrap[] GetBootstraps()
+        {
+            var bootstraps = new List<IBootstrap>();
+            bootstraps.AddRange(GetComponents<IBootstrap>());
+            bootstraps.AddRange(Bootstrap.ServiceBootstraps);
+            return bootstraps.ToArray();
+        }
+    }
+}
---
+++ Assets/Catlib/Runtime/CatLib/UnityApplication.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace CatLib
+{
+    internal sealed class UnityApplication : Application
+    {
+        /// <summary>
+        /// Unity Application
+        /// </summary>
+        /// <param name="component">根组件</param>
+        public UnityApplication(Component component)
+        {
+            Instance(Type2Service(typeof(Component)), component);
+          
+        }
+      
+    }
+}
